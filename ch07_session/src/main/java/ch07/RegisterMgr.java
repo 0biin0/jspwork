2265094ed9596e05f4695a54d0618245137974ff
hyeyeon9037 <infinite402@gmail.com>
2024-07-18 12:14:50 +0900

    0718

---
+++ ch07_session/src/main/java/ch07/RegisterMgr.java
@@ -0,0 +1,39 @@
+package ch07;
+
+import java.sql.*;
+
+public class RegisterMgr {
+	private DBConnectionMgr pool;
+	
+	public RegisterMgr() {
+		pool = DBConnectionMgr.getInstance();
+	}
+	public boolean loginRegister(String id, String pwd) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		boolean logincon = false;
+		
+		try {
+			con = pool.getConnection();
+			/*
+			 * String query = "select count(*) from mem where mem_id = " + id +
+			 * "and mem_pw = " + pwd;
+			 */
+			String query = "select count(*) from mem where mem_id = ? and mem_pw = ?";
+			pstmt = con.prepareStatement(query);
+			pstmt.setString(1, id);
+			pstmt.setString(2, pwd);
+			
+			
+			
+			
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}finally {
+			pool.freeConnection(con, pstmt, rs);
+		}
+		return logincon;
+	}
+}
