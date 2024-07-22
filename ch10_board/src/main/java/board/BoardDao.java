b80d7d8d3809f57b6992c45d36b02cf09a633a8a
hyeyeon9037 <infinite402@gmail.com>
2024-07-22 17:59:14 +0900

    0722

--- ch10_board/src/main/java/board/BoardDao.java
+++ ch10_board/src/main/java/board/BoardDao.java
@@ -1,6 +1,7 @@
 package board;
 
 import java.sql.*;
+import java.util.*;
 
 public class BoardDao {
 	private DBConnectionMgr pool;
@@ -13,4 +14,34 @@ public class BoardDao {
 	public BoardDao() {
 		pool = DBConnectionMgr.getInstance();
 	}
+
+	//게시판 리스트
+	public ArrayList<Board> getBoardList() {
+		ArrayList<Board> alist = new ArrayList<Board>();
+		try {
+			con = pool.getConnection();
+			sql = "select * from board";
+			rs = con.createStatement().executeQuery(sql);
+			while(rs.next()) {
+				Board board = new Board();
+				board.setNum(rs.getInt("num"));
+				board.setSubject(rs.getString("subject"));
+				board.setName(rs.getString("name"));
+				board.setRegdate(rs.getString("regdate"));
+				board.setCount(rs.getInt("count"));
+				board.setRef(rs.getInt("ref"));
+				board.setPos(rs.getInt("pos"));
+				board.setDepth(rs.getInt("depth"));
+				alist.add(board);
+			}
+	} catch (Exception e) {
+		e.printStackTrace();
+	} finally {
+		pool.freeConnection(con);
+	}
+	return alist;
 }
+
+}
+
+
