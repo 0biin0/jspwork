c0043d073cc619ac661b39362396345d946f5df1
hyeyeon9037 <infinite402@gmail.com>
2024-07-24 14:24:47 +0900

    0724

---
+++ ch10_board/src/main/java/board/BoardUpdateServlet.java
@@ -0,0 +1,29 @@
+package board;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+public class BoardUpdateServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String nowPage = request.getParameter("nowPage");
+		
+		Board upBean = new Board();
+		upBean.setNum(Integer.parseInt(request.getParameter("num")));
+		upBean.setName(request.getParameter("name"));
+		upBean.setSubject(request.getParameter("subject"));
+		upBean.setContent(request.getParameter("content"));
+		upBean.setPass(request.getParameter("pass"));  //read.jsp에서 사용자가 넣은 pass
+		
+		HttpSession session = request.getSession();
+		Board inBean= (Board)session.getAttribute("bean"); //DB에서 저장되어있는 pass
+	}
+
+}
